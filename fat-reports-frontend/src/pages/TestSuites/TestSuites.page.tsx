b009198e326464f5fb4d759ee13d97d78e542c04
Razvan Dragomir <drazvan91@gmail.com>
2020-11-25 19:46:02 +0200

    Add Collapse component in TestSuites page.

--- fat-reports-frontend/src/pages/TestSuites/TestSuites.page.tsx
+++ fat-reports-frontend/src/pages/TestSuites/TestSuites.page.tsx
@@ -1,5 +1,100 @@
 import * as React from 'react';
 
+import { Collapse, Select } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
+import { useMemo } from 'react';
+
+const { Panel } = Collapse;
+
+function getToolbarExtraHeader() {
+  return (
+    <SettingOutlined
+      onClick={(event) => {
+        // If you don't want click extra trigger collapse, you can prevent this:
+        event.stopPropagation();
+      }}
+    />
+  );
+}
+
+interface TestSuiteModel {
+  name: string;
+  id: string;
+  testScripts: TestScriptModel[];
+}
+
+interface TestScriptModel {
+  id: string;
+  name: string;
+}
+
+function getTestSuites(): TestSuiteModel[] {
+  return [
+    {
+      id: '1',
+      name: 'Home page test suit',
+      testScripts: [
+        {
+          id: '1',
+          name: 'Some test',
+        },
+        {
+          id: '2',
+          name: 'Some test 2',
+        },
+      ],
+    },
+    {
+      id: '32',
+      name: 'Home page test suit2',
+      testScripts: [
+        {
+          id: '1',
+          name: 'Some test',
+        },
+      ],
+    },
+    {
+      id: '453',
+      name: 'Home page test suit3',
+      testScripts: [
+        {
+          id: '1',
+          name: 'Some test',
+        },
+      ],
+    },
+    {
+      id: '123',
+      name: 'Home page test suit4',
+      testScripts: [
+        {
+          id: '1',
+          name: 'Some test',
+        },
+      ],
+    },
+  ];
+}
+
 export function TestSuitesPage() {
-  return <div>Hello from Test Suites</div>;
+  const testSuites = useMemo(() => getTestSuites(), []);
+
+  console.log('Hey there', testSuites);
+
+  return (
+    <Collapse defaultActiveKey={['1']}>
+      {testSuites.map((testSuite) => {
+        return (
+          <Panel
+            header={testSuite.name}
+            key={testSuite.id}
+            extra={getToolbarExtraHeader()}
+          >
+            <div>sd</div>
+          </Panel>
+        );
+      })}
+    </Collapse>
+  );
 }
